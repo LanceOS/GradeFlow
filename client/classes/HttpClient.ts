f042bbce7438f181dd515e515307051c3a91346c
LanceOS <lancehemphill2000@gmail.com>
2025-11-23 00:44:30 -0500

    made http client, added docker compose to server

--- client/classes/HttpClient.ts
+++ client/classes/HttpClient.ts
@@ -1,30 +1,95 @@
 
+/**
+ * 
+ */
+class ApiHttpClient {
+    private async request<TResponse, TBody = unknown>(
+        url: string,
+        method: string,
+        headers: HeadersInit,
+        body?: TBody
+    ): Promise<TResponse> {
+        const options: RequestInit = {
+          method: method,
+          headers: {
+            ...headers
+          }
+        }
+        
+        if(body !== undefined && method !== "GET" && method !== "HEAD") {
+          options.body = JSON.stringify(body);
+        }
 
-class HttpClient {
-  constructor() {}
+        try {
+          const response = await fetch(url, options);
+          if(!response.ok) {
+            throw new Error(`Response failed due to: ${response.status}: ${response.statusText}`)
+          }
 
-  private async request<TBody>(url: string, type: string, body?: TBody) {
-    return await fetch(url, {
-      method: type,
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-  }
-  
-  /*
-   * @param {string} url - An absolute URL of the API.
-   * @param {string} acceptHeader - The MIME type
-  */
-  public async get(url: string) {
-    try {
-      const response = await this.request(url, "get")
-      if(response.status === 200) {
-        return response.json()
-      }
+          return (await response.json()) as TResponse
+        }
+        catch(err: any) {
+          throw new Error(err.message, err.status)
+        }
     }
-    catch(err: any) {
-      throw new Error(err.status, err.message)
+
+    /**
+     *
+     * @param url
+     * @returns
+     */
+    public async get<TResponse>(url: string, headers: HeadersInit): Promise<TResponse> {
+        try {
+            return await this.request(url, "GET", headers);
+        } catch (err: any) {
+            throw new Error(err.message, err.status);
+        }
     }
-  } 
+
+    /**
+     *
+     * @param url
+     * @param headers
+     * @param data
+     * @returns
+     */
+    public async put<TBody>(url: string, headers: HeadersInit, data: TBody) {
+        try {
+            return await this.request(url, "PUT", headers, data);
+        } catch (err: any) {
+            throw new Error(err.message);
+        }
+    }
+
+    /**
+     * 
+     * @param url 
+     * @param headers 
+     * @param data 
+     * @returns 
+     */
+    public async post<TBody>(url: string, headers: HeadersInit, data: TBody) {
+        try {
+            return await this.request(url, "POST", headers, data);
+        } catch (err: any) {
+            throw new Error(err.message);
+        }
+    }
+
+    /**
+     * 
+     * @param url 
+     * @param headers 
+     * @param data 
+     * @returns 
+     */
+    public async delete<TBody>(url: string, headers: HeadersInit, data: TBody) {
+        try {
+            return await this.request(url, "DELETE", headers, data);
+        } catch (err: any) {
+            throw new Error(err.message);
+        }
+    }
 }
+
+export const httpClient = new ApiHttpClient();
